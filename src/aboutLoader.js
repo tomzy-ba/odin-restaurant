56e6c9b901c9ed120916693b506ef02f270c7a7d
tomzy-ba <trbee100@gmail.com>
2024-02-10 18:33:43 +0000

    Update navigation and add about page loader

---
+++ src/aboutLoader.js
@@ -0,0 +1,10 @@
+function aboutLoader() {
+    const content = document.getElementById("content");
+    
+    const aboutDiv = document.createElement("div");
+    aboutDiv.innerText = "LOOOL";
+    content.append(aboutDiv)
+    
+    }
+    
+    module.exports = aboutLoader;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const homeLoader = require("./homeLoader");
 const menuLoader = require("./menuLoader");
+const aboutLoader = require("./aboutLoader")
 
 
 homeLoader();
 
+
+const content = document.querySelector("#content");
+
 const homeButton = document.querySelector("#homeButton");
 homeButton.addEventListener("click", () => {
+    content.innerHTML = "";
     homeLoader();
 });
 
 const menuButton = document.querySelector("#menuButton");
 menuButton.addEventListener("click", () => {
-menuLoader();
+    content.innerHTML = "";
+    menuLoader();
 });
+
+const aboutButton = document.querySelector("#aboutButton");
+aboutButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    aboutLoader();
+})
